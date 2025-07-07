bd46a1d1981aa35099379504781d7d360c098a72
KirillGorinDataArt <chetanvse@gmail.com>
2025-07-07 22:30:12 +0400

    basic validation of data

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -3,6 +3,10 @@
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
+function isInvalid(text) {
+  return text?.trim() === "";
+}
+
 export async function shareMeal(formData) {
   const meal = {
     creator: formData.get("name"),
@@ -12,6 +16,20 @@ export async function shareMeal(formData) {
     instructions: formData.get("instructions"),
     image: formData.get("image"),
   };
+
+  // basic validation
+  if (
+    isInvalid(meal.title) ||
+    isInvalid(meal.summary) ||
+    isInvalid(meal.instructions) ||
+    isInvalid(meal.creator) ||
+    isInvalid(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid inputs");
+  }
 
   await saveMeal(meal);
   redirect("/meals");
